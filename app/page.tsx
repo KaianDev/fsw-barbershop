caea544dffab7a6121a772afccaef1d8dca67ec2
Kaian Vasconcelos <kaianvasconcelos@gmail.com>
2024-08-07 22:24:53 -0300

    feat: add scheduling card

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,9 @@ import { Button } from "./_components/ui/button"
 import { Input } from "./_components/ui/input"
 import { MenuIcon, SearchIcon } from "lucide-react"
 import { QUICK_SEARCH } from "./_constants/quick-search"
+import { Card, CardContent } from "./_components/ui/card"
+import { Badge } from "./_components/ui/badge"
+import { Avatar, AvatarImage } from "./_components/ui/avatar"
 
 const Home = () => {
   return (
@@ -22,18 +25,18 @@ const Home = () => {
       </div>
 
       <div className="flex gap-2 px-5">
-        <Input placeholder="Buscar" className="bg-secondary text-sm" />
+        <Input placeholder="Buscar" className="rounded-lg bg-card text-sm" />
         <Button>
           <SearchIcon size={20} />
         </Button>
       </div>
 
-      <div className="no-scrollbar flex w-full justify-between gap-2.5 overflow-x-auto pl-5 pt-6">
+      <div className="no-scrollbar flex w-full gap-2.5 overflow-x-auto pl-5 pt-6">
         {QUICK_SEARCH.map((item) => (
           <Button
             key={item.title}
-            variant="secondary"
-            className="flex w-fit shrink-0 items-center gap-2.5"
+            variant="outline"
+            className="flex w-fit shrink-0 items-center gap-2.5 bg-card"
           >
             <Image
               src={item.imageUrl}
@@ -55,6 +58,31 @@ const Home = () => {
             className="object-cover"
           />
         </div>
+      </div>
+
+      <div className="space-y-3 px-5 pt-6">
+        <h2 className="title-separator">Agendamentos</h2>
+        <Card>
+          <CardContent className="flex p-0">
+            <div className="flex flex-1 flex-col gap-3 p-3">
+              <Badge variant="tertiary">Confirmado</Badge>
+              <div className="space-x-2">
+                <h3 className="font-bold">Corte de Cabelo</h3>
+                <div className="flex items-center gap-2">
+                  <Avatar className="size-6">
+                    <AvatarImage src="https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png" />
+                  </Avatar>
+                  <p className="text-sm">Vintage Barber</p>
+                </div>
+              </div>
+            </div>
+            <div className="flex w-[106px] flex-col items-center justify-center border-l">
+              <span className="text-xs">Fevereiro</span>
+              <span className="text-2xl">06</span>
+              <span className="text-xs">09:45</span>
+            </div>
+          </CardContent>
+        </Card>
       </div>
     </div>
   )
