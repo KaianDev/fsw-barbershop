d5730a4221eba22aaeae1e2d8dc71857dedf762f
Kaian Vasconcelos <kaianvasconcelos@gmail.com>
2024-08-07 21:37:37 -0300

    feat: add initial home page

---
+++ app/_constants/quick-search.ts
@@ -0,0 +1,27 @@
+interface QuickSearch {
+  imageUrl: string
+  title: string
+}
+
+export const QUICK_SEARCH: QuickSearch[] = [
+  {
+    imageUrl: "/scissor.svg",
+    title: "Cabelo",
+  },
+  {
+    imageUrl: "/mustache.svg",
+    title: "Acabamento",
+  },
+  {
+    imageUrl: "/eyebrow.svg",
+    title: "Sobrancelha",
+  },
+  {
+    imageUrl: "/towel.svg",
+    title: "Massagem",
+  },
+  {
+    imageUrl: "/shampoo.svg",
+    title: "Hidratação",
+  },
+]
