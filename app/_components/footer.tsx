fd83a9064dede9c8a9769f9c5a34c1b1cd1ec636
Kaian Vasconcelos <kaianvasconcelos@gmail.com>
2024-08-09 23:58:30 -0300

    feat: add responsiveness to Footer component

--- app/_components/footer.tsx
+++ app/_components/footer.tsx
@@ -3,10 +3,10 @@ import { Card, CardContent } from "./ui/card"
 export const Footer = () => {
   const currentYear = new Date().getFullYear()
   return (
-    <footer className="pt-12">
+    <footer className="pt-12 md:pt-24">
       <Card className="rounded-none border-none">
         <CardContent className="p-0 px-5 py-6">
-          <p className="text-xs text-gray-400">
+          <p className="text-xs text-gray-400 md:text-end">
             &copy; {currentYear} Copyright{" "}
             <span className="font-bold">FSW Barber</span>
           </p>
