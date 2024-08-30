d1fa76a8f902f4df0583476f73949f62d4ccf737
Kaian Vasconcelos <kaianvasconcelos@gmail.com>
2024-08-30 16:55:43 -0300

    refactor: improve BarbershopsPage layout and handle empty search results

--- app/barbershops/page.tsx
+++ app/barbershops/page.tsx
@@ -56,11 +56,18 @@ const BarbershopsPage = async ({ searchParams }: BarbershopsPageProps) => {
         <h1 className="title-separator">
           Resultados para {`"${searchParams?.title || searchParams?.service}"`}
         </h1>
-        <div className="grid grid-cols-2 gap-4 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
-          {barbershops.map((barbershop) => (
-            <BarbershopItem key={barbershop.id} barbershop={barbershop} />
-          ))}
-        </div>
+        {barbershops.length > 0 && (
+          <div className="grid grid-cols-2 gap-4 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
+            {barbershops.map((barbershop) => (
+              <BarbershopItem key={barbershop.id} barbershop={barbershop} />
+            ))}
+          </div>
+        )}
+        {barbershops.length === 0 && (
+          <div className="flex h-96 items-center justify-center">
+            <p className="text-gray-500">Nenhum barbearia/serviço encontrado</p>
+          </div>
+        )}
       </div>
     </div>
   )
